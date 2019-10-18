a35ba3288ea8a29e161ed15a5795d2b75963e97b
Will Koeppen <will@axiomdatascience.com>
2019-10-18 11:56:17 -0400

    exact figure output sizes
    - revised method to provide exact figure output sizes
    - added way to handle multiple input types for resolution
    - added new error class for bad input for resolution
    - passed two tests for 1080p and 720p

--- etss_animate/basemap_image.py
+++ etss_animate/basemap_image.py
@@ -1,20 +1,56 @@
 #!/user/bin/env python
 # coding=utf-8
 
-import cartopy
-import matplotlib.pyplot as plt
-from cmocean import cm
-import math
-import matplotlib as mpl
-import numpy as np
 import io
-from PIL import Image
+import math
 import pickle
 
+import cartopy
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+from cmocean import cm
+from PIL import Image
 
-def serialize_to_image(in_figure_object):
+
+class ResolutionValueError(ValueError):
+    pass
+
+
+def get_resolution(arg):
+    """Handle the resolution argument."""
+    if isinstance(arg, tuple):
+        return arg
+    if isinstance(arg, str):
+        if arg == "1080p":
+            return (1920, 1080)
+        if arg == "720p":
+            return (1080, 720)
+        else:
+            # I feel like this isn't the correct error to raise
+            raise ResolutionValueError(
+                "Valid inputs are '1080p', "
+                + "'720p', or a two-element tuple (width, height)."
+            )
+
+
+def resolution_to_figsize(_input_tuple, dpi=72):
+    """Convert pixel resolution to inches for input into matplotlib
+    figsize."""
+
+    width_px = _input_tuple[0]
+    height_px = _input_tuple[1]
+
+    width_inch = width_px / dpi
+    height_inch = height_px / dpi
+
+    return (width_inch, height_inch)
+
+
+def serialize_to_buffer(in_figure_object, dpi=72):
+    """Write the matplotlib figure object to a PNG in a buffer."""
     out_buffer = io.BytesIO()
-    in_figure_object.savefig(out_buffer, bbox_inches="tight", format="PNG")
+    in_figure_object.savefig(out_buffer, dpi=dpi, format="PNG")
     out_image = Image.open(out_buffer)
 
     return out_image
@@ -27,19 +63,24 @@ def serialize_to_image(in_figure_object):
 #     return out_buffer
 
 
-def make_basemap(limit=0, extent=False):
+def make_basemap(limit=0, extent=(170, 255, 40, 76), resolution="1080p"):
     """
     Return a basemap image (PNG) with a coastline and rivers.
     
     Parameters
     ----------
-    extent: (float, float, float, float), default:False
+    extent: (float, float, float, float), default:(170, 255, 40, 76)
         Tuple of floats representing the required extent
         (x0, x1, y0, y1)
 
     limit: integer, default: 0
         The limits of the legend, which should match the
         gridded data
+
+    resolution: (float, float), "1080p", "720p"
+        A tuple dictating the width and height of the output. In
+        addition, some strings are accepted. "1080p" is an alias
+        for (1920, 1080), and "720p" is aliased to (1080, 720).
     
     Returns
     -------
@@ -49,6 +90,7 @@ def make_basemap(limit=0, extent=False):
     plate_crs = cartopy.crs.PlateCarree()
     merc_crs = cartopy.crs.Mercator(central_longitude=210)
 
+    dpi = 72
     land_color = "#fcfcfc"
     coast_color = "#b1b1b1"
     # ocean_color = "#d7dfe0"
@@ -56,12 +98,12 @@ def make_basemap(limit=0, extent=False):
     # annotation_spacing = 0.2
     cmap = cm.balance
 
-    fig = plt.figure(dpi=72, figsize=(14, 14))
-    ax = fig.add_axes([0, 0, 1, 1], projection=merc_crs)
-    ax.outline_patch.set_visible(False)
+    out_resolution = get_resolution(resolution)
+    fig_size = resolution_to_figsize(out_resolution)
 
-    if not extent:
-        extent = (170, 255, 40, 76)
+    fig = plt.figure(dpi = dpi, figsize = (fig_size[0], fig_size[1]))
+    ax = fig.add_axes([0, 0, 1, 1], projection = merc_crs)
+    ax.outline_patch.set_visible(False)
 
     ### Contextual Map Elements ###
     land_10m = cartopy.feature.NaturalEarthFeature(
@@ -88,19 +130,19 @@ def make_basemap(limit=0, extent=False):
     vmax = limit
 
     ### ETSS Color Ramp ###
-    norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
-    cax = fig.add_axes([0.91, 0.09, 0.02, 0.20])  # colorbar
-    cb = mpl.colorbar.ColorbarBase(
-        cax, cmap=cmap, norm=norm, orientation="vertical", extend="both"
-    )
-    cb.outline.set_visible(False)
-    cb.set_label("Predicted Height in meters (NAVD88)", fontsize=13)
-    cb.set_ticks(np.arange(vmin, vmax + 1, np.abs((vmin - vmax) / 4)))
+    # norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
+    # cax = fig.add_axes([0.91, 0.09, 0.02, 0.20])  # colorbar
+    # cb = mpl.colorbar.ColorbarBase(
+    #     cax, cmap=cmap, norm=norm, orientation="vertical", extend="both"
+    # )
+    # cb.outline.set_visible(False)
+    # cb.set_label("Predicted Height in meters (NAVD88)", fontsize=13)
+    # cb.set_ticks(np.arange(vmin, vmax + 1, np.abs((vmin - vmax) / 4)))
 
     # Write to image file
-    im = serialize_to_image(fig)
+    im = serialize_to_buffer(fig, dpi=dpi)
 
-#    pic = serialize_to_pickle(fig)
+    #    pic = serialize_to_pickle(fig)
     # Save as a pickle file
     #     pickle.dump(fig, base_fig_buf)
     #     plt.close(fig)
--- tests/test_basemap_image.py
+++ tests/test_basemap_image.py
@@ -4,6 +4,22 @@ from PIL import ImageFile
 # Is the output an image file?
 def test_base():
     base = basemap_image.make_basemap(limit=2, extent=(170, 255, 40, 76))
+
     assert isinstance(base, ImageFile.ImageFile)
 
-#Does it have the correct size given input?
+
+# Does it have the correct size given input?
+def test_1080p():
+    base = basemap_image.make_basemap(
+        limit=2, extent=(170, 255, 40, 76), resolution="1080p"
+    )
+
+    assert base.size == (1920, 1080)
+
+
+def test_720p():
+    base = basemap_image.make_basemap(
+        limit=2, extent=(170, 255, 40, 76), resolution="720p"
+    )
+
+    assert base.size == (1080, 720)
